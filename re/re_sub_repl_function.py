c426dd8634ce344019f513ad4baaef0ee9f15725
haibin0332 <haibin.zhang@students.mq.edu.au>
2015-06-07 18:50:47 +1000

    Update re_sub_repl_function.py

--- re/re_sub_repl_function.py
+++ re/re_sub_repl_function.py
@@ -34,16 +34,5 @@ inputStr = "hello crifan, nihao abdgg";
 replacedStr = re.sub(r"hello (\w+), nihao", "\g<1>", inputStr); #\g<1> group 1
 print "replacedStr=",replacedStr; #crifan  
 #output replacedStr= crifan abdgg
-############################################################################
-from urllib import urlopen
-import re
 
-webpage= urlopen('https://www.python.org/')
-text=webpage.read()
-m=re.search('<a href="([^"]+)" .*?>about</a>', text, re.IGNORECASE)
-print m.group()
-print m.group(1)
-
-#output <a href="/about/" title="" class="">About</a>
-#output /about/
 
