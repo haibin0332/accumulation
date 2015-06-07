fbda02359e06e95b428b64d8b706083d19276528
haibin0332 <haibin.zhang@students.mq.edu.au>
2015-06-07 19:58:07 +1000

    Update re_urltest.py

--- re/re_urltest.py
+++ re/re_urltest.py
@@ -1,23 +1,23 @@
 #this example simply download sina html and save to local c:\sina.html
-#import urllib
+import urllib
 
-# def callbackfunc(blocknum, blocksize, totalsize):
-#     '''
-#     @blocknum: the block have already been downloaded
-#     @blocksize: 
-#     @totalsize: 
-#     '''
-#     percent = 100.0 * blocknum * blocksize / totalsize
-#     if percent > 100:
-#         percent = 100
-#     #print "%.2f"% percent # 1.37
-#     print "%.2f%%" %percent #1.37%
-# #remember to use r'' to avoid error
-# url = r'http://www.sina.com.cn'
-# #first set up a sina.html in local "C:\"
-# local = r'C:\sina.html'
-# #from urllib import urlretrieve # use urlretrieve(url, local, callbackfunc)
-# urllib.urlretrieve(url, local, callbackfunc)
+def callbackfunc(blocknum, blocksize, totalsize):
+     '''
+     @blocknum: the block have already been downloaded
+     @blocksize: 
+     @totalsize: 
+     '''
+     percent = 100.0 * blocknum * blocksize / totalsize
+     if percent > 100:
+         percent = 100
+     #print "%.2f"% percent # 1.37
+     print "%.2f%%" %percent #1.37%
+ #remember to use r'' to avoid error
+ url = r'http://www.sina.com.cn'
+ #first set up a sina.html in local "C:\"
+ local = r'C:\sina.html'
+ #from urllib import urlretrieve # use urlretrieve(url, local, callbackfunc)
+ urllib.urlretrieve(url, local, callbackfunc)
 # 
 # 
 # ############################################################################
